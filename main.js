c00877563aa6cfe947240ddefb34a7d519807fc2
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-06-18 23:32:07 +0100

    Added function to reprompt users if they don't input a number

--- main.js
+++ main.js
@@ -2,29 +2,53 @@ var option = prompt('Please enter ADD, SUBTRACT, MULTIPLY or DIVIDE, or enter QU
 
 while (option.toUpperCase() !== 'QUIT' ) {
 	if (option.toUpperCase() == 'ADD') {
-		var number1 = prompt('Please enter your first number');
+		var number1 = prompt('Please enter your first number.');
+		while (isNaN(number1) || number1 == '') {
+			var number1  = prompt(number1 + ' is not a number. Please enter a number.');
+		}
 		var number2 = prompt('Please enter your second number');
+		while (isNaN(number2) || number2 == '') {
+			var number2 = prompt(number2 + ' is not a number. Please enter a number.');
+		}
 		var result = parseFloat(number1) + parseFloat(number2);
 		alert('The result is: ' + result);
 		var option = prompt('Please enter ADD, SUBTRACT, MULTIPLY or DIVIDE, or enter QUIT to exit the calculator.');
 	}
 	else if (option.toUpperCase() == 'SUBTRACT') {
-		var number1 = prompt('Please enter your first number');
+		var number1 = prompt('Please enter your first number.');
+		while (isNaN(number1) || number1 == '') {
+			var number1  = prompt(number1 + ' is not a number. Please enter a number.');
+		}
 		var number2 = prompt('Please enter your second number');
+		while (isNaN(number2) || number2 == '') {
+			var number2 = prompt(number2 + ' is not a number. Please enter a number.');
+		}
 		var result = parseFloat(number1) - parseFloat(number2);
 		alert('The result is: ' + result);
 		var option = prompt('Please enter ADD, SUBTRACT, MULTIPLY or DIVIDE, or enter QUIT to exit the calculator.');
 	}
 	else if (option.toUpperCase() == 'MULTIPLY') {
-		var number1 = prompt('Please enter your first number');
+		var number1 = prompt('Please enter your first number.');
+		while (isNaN(number1) || number1 == '') {
+			var number1  = prompt(number1 + ' is not a number. Please enter a number.');
+		}
 		var number2 = prompt('Please enter your second number');
+		while (isNaN(number2) || number2 == '') {
+			var number2 = prompt(number2 + ' is not a number. Please enter a number.');
+		}
 		var result = parseFloat(number1) * parseFloat(number2);
 		alert('The result is: ' + result);
 		var option = prompt('Please enter ADD, SUBTRACT, MULTIPLY or DIVIDE, or enter QUIT to exit the calculator.');
 	}
 	else if (option.toUpperCase() == 'DIVIDE') {
-		var number1 = prompt('Please enter your first number');
+		var number1 = prompt('Please enter your first number.');
+		while (isNaN(number1) || number1 == '') {
+			var number1  = prompt(number1 + ' is not a number. Please enter a number.');
+		}
 		var number2 = prompt('Please enter your second number');
+		while (isNaN(number2) || number2 == '') {
+			var number2 = prompt(number2 + ' is not a number. Please enter a number.');
+		}
 		var result = parseFloat(number1) / parseFloat(number2);
 		alert('The result is: ' + result);
 		var option = prompt('Please enter ADD, SUBTRACT, MULTIPLY or DIVIDE, or enter QUIT to exit the calculator.');
@@ -33,3 +57,4 @@ while (option.toUpperCase() !== 'QUIT' ) {
 		var option = prompt('Sorry, that command is not recognised. Please enter ADD, SUBTRACT, MULTIPLY or DIVIDE, or enter QUIT to exit the calculator.');
 	}
 };
+
